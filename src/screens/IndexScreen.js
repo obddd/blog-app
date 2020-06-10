9f2a4a181e5b330ca6645ef663dfe764ef821bc1
Obaid <obaidulhaq20@gmail.com>
2020-06-11 02:38:14 +0500

    automated context creation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+
+import {Context} from '../context/BlogContext';
 
 const IndexScreen = () => {
-    return(
-        <View>
-            <Text>Index screen</Text>
-        </View>
-    )
-}
+  const { state, addBlogPosts } = useContext(Context);
+  return (
+    <View>
+      <Text>Index screen</Text>
+      <Button title='Add Posts' onPress={addBlogPosts}/>
+      <FlatList
+        data={state}
+        keyExtractor={(blog) => blog.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({});
 
